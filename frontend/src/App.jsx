ebf34b6be1b85f01362326a95a1b0935d7adb087
Anshul Kanwar <4n5hu1@protonmail.com>
2023-02-26 01:16:44 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import Navbar from './Navbar'
+import Navbar from "./components/Navbar";
 import Inp from "./Inp";
 
-
-
 const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Inp />
-  )
-}
+    <div>
+      <Navbar />
+      <main className="max-w-xl mx-auto mt-48">
+        <div className="text-center text-slate-600">
+          Upload two .obj files below and our algorithm will tell you if the 3d Model has been plagiarised
+        </div>
+        <div className="p-10 mt-8 bg-white rounded-lg">
+          <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-5">
+            <label>Original File: </label>
+            <input type="file" name="file1" />
+            <label>Second File: </label>
+            <input type="file" name="file2" />
+            <button
+              type="submit"
+              className="col-span-2 bg-blue-600 text-white rounded-md p-3"
+            >
+              Check for Plagiarism
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
