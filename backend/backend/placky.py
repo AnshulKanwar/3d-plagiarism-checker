9aebd9ca1743087c24365a9d67e4dc6be462418c
arjundvn24 <adhawan_be20@thapar.edu>
2023-02-26 04:59:52 +0530

    req.txt

--- backend/backend/placky.py
+++ backend/backend/placky.py
@@ -24,15 +24,13 @@ def similarity():
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
     file = request.files['file1']
-    # if allowed_file(file):
     new_file_name="model1.obj"
     file.save('uploads/' + new_file_name)
     file = request.files['file2']
-    # if allowed_file(file):  
     new_file_name="model2.obj"
     file.save('uploads/' + new_file_name)
     if 'file1' not in request.files and 'file2' not in request.files:
         return jsonify(error="Obj file not provided")
-    # os.system("blender -b -P script.py")
-    # result = ml.delay().get()
-    return jsonify(similarity=1)
+    os.system("blender -b -P uploads/script.py")
+    result = ml.delay().get()
+    return jsonify(similarity=result)
