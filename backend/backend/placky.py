5ac7d8dfe5d8b8d321f53971d6859819391317c2
arjundvn24 <adhawan_be20@thapar.edu>
2023-02-26 03:33:22 +0530

    changes

--- backend/backend/placky.py
+++ backend/backend/placky.py
@@ -1,7 +1,8 @@
 from flask import (
     Blueprint, request, jsonify,render_template
 )
-# from tasks import ml
+from tasks import ml
+import os
 
 bp = Blueprint('placky', __name__, url_prefix='/')
 
@@ -20,7 +21,18 @@ def allowed_file(filename):
 
 @bp.route("/", methods=('POST', ))
 def similarity():
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+    file = request.files['file1']
+    # if allowed_file(file):
+    new_file_name="model1.obj"
+    file.save('uploads/' + new_file_name)
+    file = request.files['file2']
+    # if allowed_file(file):  
+    new_file_name="model2.obj"
+    file.save('uploads/' + new_file_name)
     if 'file1' not in request.files and 'file2' not in request.files:
         return jsonify(error="Obj file not provided")
+    # os.system("blender -b -P script.py")
     # result = ml.delay().get()
-    return jsonify(similarity=0.7)
+    return jsonify(similarity=1)
--- backend/uploads/script.py
+++ backend/uploads/script.py
@@ -1,10 +1,10 @@
 import bpy
 import math
+import open3d as o3d
+import numpy as np
 
-def align_camera(camera, dir):
-    
+def align_camera(camera, dir):    
     distance = 2
-    
     if dir == "top":
         camera.location = (0, 0, distance)
         camera.rotation_euler = (0, 0, 0)
@@ -25,18 +25,26 @@ def align_camera(camera, dir):
         camera.rotation_euler = (0, math.radians(-90), 0)
 
 def take_snapshot(filename):
-    bpy.context.scene.render.filepath = f"/Users/anshulkanwar/Developer/mlsc-makeathon/script/{filename}"
+    bpy.context.scene.render.filepath = f"./images/{filename}"
     bpy.ops.render.render(write_still=True)
     
 def main():
     bpy.data.objects['Cube'].select_set(True)
     bpy.ops.object.delete()
 
-    obj = bpy.ops.import_scene.obj(filepath="model_normalized.obj")
+    # obj1 = bpy.ops.import_scene.obj(filepath="model1.obj")
+    # mesh = o3d.io.read_triangle_mesh(obj1)
+    # vertices = np.asarray(mesh.vertices)
+    # centroid = np.mean(vertices, axis=0)
+    # vertices -= centroid
+    # max_distance = np.max(np.linalg.norm(vertices, axis=1))
+    # vertices /= max_distance
+    # mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    # o3d.io.write_triangle_mesh("model1.obj", mesh)
     camera = bpy.data.objects['Camera']
     
     for dir in ["top", "bottom", "front", "back", "right", "left"]:
         align_camera(camera, dir)
-        take_snapshot(f"./output/{dir}.png")
+        take_snapshot(f"{dir}.png")
 
 main()
