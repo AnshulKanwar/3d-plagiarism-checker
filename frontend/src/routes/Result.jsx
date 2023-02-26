3da422e7bb9144588cc87ae08eafd85b550a15e5
arjundvn24 <adhawan_be20@thapar.edu>
2023-02-26 11:22:24 +0530

    images added

--- frontend/src/routes/Result.jsx
+++ frontend/src/routes/Result.jsx
@@ -40,13 +40,13 @@ const Result = () => {
         </div>
         <div className="mt-20">
           <h1 className="text-xl font-bold my-2">Similarities</h1>
-          <div className="grid grid-cols-2 grid-rows-3 gap-5">
-            <div className="bg-slate-300 w-full aspect-square"></div>
-            <div className="bg-slate-300 w-full aspect-square"></div>
-            <div className="bg-slate-300 w-full aspect-square"></div>
-            <div className="bg-slate-300 w-full aspect-square"></div>
-            <div className="bg-slate-300 w-full aspect-square"></div>
-            <div className="bg-slate-300 w-full aspect-square"></div>
+          <div className="grid grid-cols-1 gap-5">
+            {[1, 2, 3, 4, 5, 6].map((idx) => (
+              <img
+                className="bg-slate-300 h-96  w-auto"
+                src={`http://127.0.0.1:5000/results/${idx}.jpg`}
+              ></img>
+            ))}
           </div>
         </div>
       </main>
